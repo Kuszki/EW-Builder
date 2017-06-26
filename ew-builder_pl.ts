9a78223041eab0dbde0f4c76310d3e7c05584305
Kuszki <l.drozdz@openmailbox.org>
2017-06-26 18:47:25 +0200

    Add translations

---
+++ ew-builder_pl.ts
@@ -0,0 +1,299 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="aboutdialog.ui" line="20"/>
+        <source>About</source>
+        <translation>O programie</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="30"/>
+        <source>Exit</source>
+        <translation>Zamknij</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="71"/>
+        <source>Program name:</source>
+        <translation>Program:</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="88"/>
+        <source>Author:</source>
+        <translation>Autor:</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="105"/>
+        <source>Mail:</source>
+        <translation>E-mail:</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="125"/>
+        <source>Project slite:</source>
+        <translation>Strona projektu:</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="145"/>
+        <source>License:</source>
+        <translation>Licencja:</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="165"/>
+        <source>Powered by:</source>
+        <translation>Wsparcie:</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="187"/>
+        <source>This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+The program is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.</source>
+        <translation>Ten program jest wolnym oprogramowaniem: możesz go rozpowszechniać i/lub modyfikować na warunkach Powszechnej Publicznej Licencji GNU GPL opublikowanej przez Fundacje Wolnego Oprogramowania w wersji 3 lub (według uznania) dowolnej późniejszej.
+
+Niniejszy program rozpowszechniany jest z nadzieją, iż będzie on użyteczny – jednak BEZ ŻADNEJ GWARANCJI, nawet domyślnej gwarancji PRZYDATNOŚCI HANDLOWEJ albo PRZYDATNOŚCI DO OKREŚLONYCH ZASTOSOWAŃ. Bliższe informacje na ten temat można uzyskać z Powszechnej Licencji Publicznej GNU.</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="202"/>
+        <source>&lt;a href=&quot;http://www.fsf.org/&quot;&gt;See more&lt;/a&gt;</source>
+        <translation>&lt;a href=&quot;http://www.fsf.org/&quot;&gt;Dowiedz się więcej&lt;/a&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>ConnectDialog</name>
+    <message>
+        <location filename="connectdialog.ui" line="14"/>
+        <source>Open database</source>
+        <translation>Otwórz baze danych</translation>
+    </message>
+    <message>
+        <location filename="connectdialog.ui" line="24"/>
+        <source>Database</source>
+        <translation>Baza danych</translation>
+    </message>
+    <message>
+        <location filename="connectdialog.ui" line="31"/>
+        <source>Password</source>
+        <translation>Hasło</translation>
+    </message>
+    <message>
+        <location filename="connectdialog.ui" line="74"/>
+        <source>User</source>
+        <translation>Użytkownik</translation>
+    </message>
+    <message>
+        <location filename="connectdialog.ui" line="84"/>
+        <source>Server</source>
+        <translation>Serwer</translation>
+    </message>
+    <message>
+        <location filename="connectdialog.cpp" line="81"/>
+        <source>Error</source>
+        <translation>Błąd</translation>
+    </message>
+</context>
+<context>
+    <name>DatabaseDriver</name>
+    <message>
+        <location filename="databasedriver.cpp" line="41"/>
+        <source>Job name</source>
+        <translation>KERG</translation>
+    </message>
+    <message>
+        <location filename="databasedriver.cpp" line="42"/>
+        <source>Object code</source>
+        <translation>Kod obiektu</translation>
+    </message>
+    <message>
+        <location filename="databasedriver.cpp" line="43"/>
+        <source>Object ID</source>
+        <translation>Identyfikator obiektu</translation>
+    </message>
+    <message>
+        <location filename="databasedriver.cpp" line="44"/>
+        <source>Creation date</source>
+        <translation>Data utworzenia</translation>
+    </message>
+    <message>
+        <location filename="databasedriver.cpp" line="45"/>
+        <source>Modification date</source>
+        <translation>Data modyfikacji</translation>
+    </message>
+    <message>
+        <location filename="databasedriver.cpp" line="46"/>
+        <source>Created by</source>
+        <translation>Utworzony przez</translation>
+    </message>
+    <message>
+        <location filename="databasedriver.cpp" line="47"/>
+        <source>Modified by</source>
+        <translation>Zmodyfikowany przez</translation>
+    </message>
+    <message>
+        <location filename="databasedriver.cpp" line="146"/>
+        <location filename="databasedriver.cpp" line="184"/>
+        <location filename="databasedriver.cpp" line="1324"/>
+        <source>Unknown</source>
+        <translation>Niezdefiniowane</translation>
+    </message>
+    <message>
+        <location filename="databasedriver.cpp" line="1106"/>
+        <source>Loading database informations</source>
+        <translation>Wczytywanie informacji o bazie danych</translation>
+    </message>
+    <message>
+        <location filename="databasedriver.cpp" line="1137"/>
+        <source>Database is not opened</source>
+        <translation>Baza danych nie jest otwarta</translation>
+    </message>
+    <message>
+        <location filename="databasedriver.cpp" line="1172"/>
+        <source>Preparing geometry</source>
+        <translation>Opracowywanie geometrii</translation>
+    </message>
+    <message>
+        <location filename="databasedriver.cpp" line="1183"/>
+        <source>Preparing attributes</source>
+        <translation>Opracowywanie atrybutów</translation>
+    </message>
+    <message>
+        <location filename="databasedriver.cpp" line="1218"/>
+        <source>Creating objects</source>
+        <translation>Tworzenie obiektów</translation>
+    </message>
+    <message>
+        <location filename="databasedriver.cpp" line="1306"/>
+        <source>Yes</source>
+        <translation>Tak</translation>
+    </message>
+    <message>
+        <location filename="databasedriver.cpp" line="1306"/>
+        <source>No</source>
+        <translation>Nie</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>EW-Builder</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="31"/>
+        <source>Line layer</source>
+        <translation>Warstwa liniowa</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="38"/>
+        <source>Label pattern</source>
+        <translation>Wzorzec etykiety</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="78"/>
+        <source>Class</source>
+        <translation>Kod obiektu</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="97"/>
+        <source>Point layer</source>
+        <translation>Warstwa punktowa</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="110"/>
+        <source>Text layer</source>
+        <translation>Warstwa opisowa</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="118"/>
+        <source>Tools</source>
+        <translation>Narzędzia</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="139"/>
+        <source>Support</source>
+        <translation>Wsparcie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="167"/>
+        <source>About</source>
+        <translation>O programie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="170"/>
+        <source>F1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="179"/>
+        <source>Connect</source>
+        <translation>Połącz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="182"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="191"/>
+        <source>Disconnect</source>
+        <translation>Rozłącz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="194"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="203"/>
+        <source>Proceed</source>
+        <translation>Przetwórz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="206"/>
+        <source>Ctrl+R</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="214"/>
+        <source>Cancel</source>
+        <translation>Zatrzymaj</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="217"/>
+        <source>Esc</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="mainwindow.cpp" line="219"/>
+        <source>Created %n item(s)</source>
+        <translation>
+            <numerusform>Utworzono %n obiekt</numerusform>
+            <numerusform>Utworzono %n obiekty</numerusform>
+            <numerusform>Utworzono %n obiektów</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>UpdateWidget</name>
+    <message>
+        <location filename="updatewidget.ui" line="51"/>
+        <source>Expression</source>
+        <translation>Wyrażenie regularne</translation>
+    </message>
+    <message>
+        <location filename="updatewidget.cpp" line="147"/>
+        <source>Select values</source>
+        <translation>Zaznacz wartości</translation>
+    </message>
+    <message>
+        <location filename="updatewidget.cpp" line="201"/>
+        <source>Yes</source>
+        <translation>Tak</translation>
+    </message>
+    <message>
+        <location filename="updatewidget.cpp" line="202"/>
+        <source>No</source>
+        <translation>Nie</translation>
+    </message>
+</context>
+</TS>
